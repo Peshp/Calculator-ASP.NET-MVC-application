67a27b02b0e902d3d9dd0f0fc8c31b25682f0a1a
af 1 proekt <af 1 proekt@DESKTOP-93KS4NV>
2022-08-08 14:15:23 +0300

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Numbers()
+        {           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Numbers(int number)
+        {           
+            ViewBag.Number = number;          
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(double firstNumber, string operation, double secondNumber)
+        {
+            string result = string.Empty;
+
+            switch (operation)
+            {
+                case "+":
+                    result = $"{firstNumber + secondNumber}";
+                    break;
+                case "-":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                case "*":
+                    result = $"{firstNumber * secondNumber}";
+                    break;
+                case "/":
+                    result = $"{firstNumber / secondNumber}";
+                    break;
+                case "%":
+                    result = $"{firstNumber % secondNumber}";
+                    break;
+                default:
+                    result = "Invalid operation";
+                    break;
+            }
+
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
